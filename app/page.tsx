147678212f0eafcf340eba24f36e177ec570fbf2
Nana Kofi Larbi Mantey <nanakofilarbimantey@gmail.com>
2023-04-13 21:47:33 +0000

    removed default styles

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <main className={`${inter.className}`}>
       <div className="px-8 py-4">
-        <h2>
+        <h2 className="font-semibold">
           this is a starter repo for a Next.js 13 web app using the experimental
           app dir
         </h2>
